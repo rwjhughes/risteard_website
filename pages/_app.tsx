251bd10881d4d797b73758a04ad8ec168bf4b941
rwjhughes <richard@hughes.ie>
2023-10-06 12:46:45 +0100

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import Enter from '@/components/enter'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <Component {...pageProps} />
     <Enter />
+    <Analytics />
   </>
 }
